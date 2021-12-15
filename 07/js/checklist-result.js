1241a392008fda1d67b6d2da4873029c10933673
Woo <swoo0naver.com>
2021-12-15 18:02:09 +0900

    test

--- 07/js/checklist-result.js
+++ 07/js/checklist-result.js
@@ -7,7 +7,7 @@
 			var item = document.querySelector("#item").value;  // 텍스트 필드 내용 가져옴
 			if (item != null) {
 				itemList.push(item);  // itemList 배열의 끝에 item 변수 값 추가
-				document.querySelector("#item").value = "";  // id=”item”인 요소의 값을 지움
+				document.querySelector("#item").value = null;  // id=”item”인 요소의 값을 지움
 				document.querySelector("#item").focus();  // 텍스트 필드에 focus( ) 메서드 적용
 			}
 			showList();
