f2646280be26008ed05ea62186fda46b40586558
Woo <swoo0naver.com>
2021-12-17 11:58:41 +0900

    chapter8 start

--- 08/js/domevent.js
+++ 08/js/domevent.js
@@ -1,8 +1,17 @@
     var pic = document.querySelector('#pic');
     
+    pic.addEventListener("mouseover", changePic, false);
+    pic.addEventListener("mouseout", originPic, false);
+
 		function changePic() {			
 			pic.src = "images/boy.png";
     }
     function originPic() {
       pic.src = "images/girl.png";
     }
+
+    var picc = document.querySelector('document');
+
+    document.addEventListener('click', function(){
+      alert("Hello");
+    })
